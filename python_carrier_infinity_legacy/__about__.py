84b5a1672f983f95bbe096126417f3c30a62ef33
@mileswu <mileswu@users.noreply.github.com>
2023-08-07 22:06:14 +0000

    Remove legacy code

--- python_carrier_infinity_legacy/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present @mileswu <mileswu@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
--- python_carrier_infinity_legacy/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present @mileswu <mileswu@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
-from .location import Location
-from .system import System, systems
-from .api import Auth
